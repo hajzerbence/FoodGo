c642ea0c7591baadde6142bb9483034e6c285e76
Czeglédi Máté <czegledimate06@gmail.com>
2026-01-21 18:11:25 +0100

    Éttermek hozzáadása

--- frontend/Főoldal/script.js
+++ frontend/Főoldal/script.js
@@ -1,25 +1,44 @@
+//Megfogjuk a szűrő gombjait Id alapján
 const gombAmerikai = document.getElementById('gombAmerikai');
 const gombAzsiai = document.getElementById('gombAzsiai');
 const gombTorok = document.getElementById('gombTorok');
 const gombOsszes = document.getElementById('gombOsszes');
 
-const amerikaiKartya = document.getElementById('amerikaiKartya');
-const azsiaiKartya = document.getElementById('azsiaiKartya');
-const torokKartya = document.getElementById('torokKartya');
+// Megfogjuk a kártyákat class alapján
+const amerikaiKartya = document.getElementsByClassName('amerikaiKartya');
+const azsiaiKartya = document.getElementsByClassName('azsiaiKartya');
+const torokKartya = document.getElementsByClassName('torokKartya');
 
+// Ez a függvény megmutat minden kártyát. Leveszi róluk a 'display: none' stílust, ha van rajtuk, így újra láthatóvá teszi őket.
 function mindentMutat() {
-    amerikaiKartya.style.display = '';
-    azsiaiKartya.style.display = '';
-    torokKartya.style.display = '';
+    for (var i = 0; i < amerikaiKartya.length; i++) {
+        amerikaiKartya[i].style.display = '';
+    }
+    for (var i = 0; i < azsiaiKartya.length; i++) {
+        azsiaiKartya[i].style.display = '';
+    }
+    for (var i = 0; i < torokKartya.length; i++) {
+        torokKartya[i].style.display = '';
+    }
 }
 
+// Ez a függvény csak a megadott kategóriájú kártyákat mutatja meg. Az összes kártyára 'display: none' stílust állít be, majd a megadott kategóriájú kártyákból leveszi ezt a stílust, így csak azok lesznek láthatóak.
 function csakEztMutat(kartya) {
-    amerikaiKartya.style.display = 'none';
-    azsiaiKartya.style.display = 'none';
-    torokKartya.style.display = 'none';
-    kartya.style.display = '';
+    for (var i = 0; i < amerikaiKartya.length; i++) {
+        amerikaiKartya[i].style.display = 'none';
+    }
+    for (var i = 0; i < azsiaiKartya.length; i++) {
+        azsiaiKartya[i].style.display = 'none';
+    }
+    for (var i = 0; i < torokKartya.length; i++) {
+        torokKartya[i].style.display = 'none';
+    }
+    for (var i = 0; i < kartya.length; i++) {
+        kartya[i].style.display = '';
+    }
 }
 
+// Függvények meghívása a megfelelő gombok kattintására
 document.addEventListener('DOMContentLoaded', function () {
     mindentMutat();
 
